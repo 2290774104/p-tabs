a96e64150fa61d37e31c7af58cbf1a66184a8713
唐皓晨 <2290774104@qq.com>
2022-02-22 16:18:00 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+
+import 'normalize.css'
+import ElementUI from 'element-ui'
+import SvgIcon from 'vue-svgicon'
+
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/styles/index.scss'
+
+import App from '@/App.vue'
+import '@/icons/components'
+
+Vue.use(ElementUI)
+Vue.use(SvgIcon, {
+  tagName: 'svg-icon',
+  defaultWidth: '1em',
+  defaultHeight: '1em'
+})
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
---
+++ types/index.d.ts
@@ -0,0 +1,8 @@
+
+import Vue from 'vue'
+// for future
+// export interface InstallationOptions {}
+// export function install (vue: typeof Vue, options: InstallationOptions): void
+
+// now
+export function install(vue: typeof Vue): void
